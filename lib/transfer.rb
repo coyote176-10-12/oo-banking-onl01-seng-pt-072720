3afb62eda2fcb6c1431e67ef87492d06a33f864c
Javier Aguilar <coyote176@msn.com>
2020-08-24 03:32:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   
   attr_accessor :sender, :receiver, :status, :amount
   
-  def initialize(sender, receiver, transfer)
+  def initialize(sender, receiver, amount, transfer)
     @sender= sender
     @receiver= receiver
     @status= "pending"
